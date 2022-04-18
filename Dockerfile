e86bcf311a8d6b4fb47bd1bb796f6c2bf27ffb7e
wak10 <chiba.r25.taka@gmail.com>
2022-04-18 15:43:34 +0900

    [add]Dockerfile記載

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM ruby :2.7
+
+RUN mkdir /var/www
+COPY main.rb /var/www
+CMD ["ruby", "/var/www/main.rb"]
---
+++ main.rb
@@ -0,0 +1,11 @@
+require "webrick"
+
+server = WEBrick::HTTPServer.new(
+  DocumentRoot: "./",
+  BindAddres: "0.0.0.0",
+  Port: 8000
+)
+
+server.mount_proc("/") do |req,res|
+  res.body = "hello"
+end
